976cb75c545d1fc20d659296b2b4f240a631d3be
gengrui <gengr.r@gmail.com>
2016-04-21 03:04:27 +0000

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  before_save { self.email = email.downcase }
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  
+  validates :email, presence: true, length: { in: 3..50 },
+            uniqueness: { case_sensitive: false },
+            format: { with: Devise.email_regexp }, on: [:save]
+  
+  validates :password, presence: true, confirmation: true,
+            length: { in: Devise.password_length }, on: [:save]
+  
+  validates :password_confirmation, :presence => true,
+            length: { in: Devise.password_length }, on: [:save]
 end
